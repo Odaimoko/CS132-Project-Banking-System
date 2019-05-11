d0428d61a6e0d91620067c29649e01155250d772
Imoko Oda <odaimoko@qq.com>
2019-05-11 16:55:03 +0800

    create database

--- Banking_system.m
+++ Banking_system.m
@@ -6,6 +6,7 @@ classdef Banking_system
         ui_manager;
         functional;
         db_ticket;
+        db_clerk;
         db_account;
     end
     
@@ -13,13 +14,36 @@ classdef Banking_system
         function obj = Banking_system(inputArg1,inputArg2)
             %BANKING_SYSTEM 构造此类的实例
             %   此处显示详细说明
-            obj.Property1 = inputArg1 + inputArg2;
+%             Read existing or create database
+
+
+            if ~exist('data','dir')
+                mkdir('data')
+            end
+            if exist('data/sys.mat','file')
+                load 'data/sys.mat'
+            else
+                % init
+                obj.db_account= containers.Map;
+                obj.db_account('nextAccountID')=1;
+                obj.db_account('accounts')=containers.Map; % person id maps to list of accounts
+                obj.db_ticket=0;
+            end
+            % initialize ticket account in mem
+            % init clerk 
+            save 'data/sys.mat' obj
         end
         
-        function outputArg = method1(obj,inputArg)
+    %     function outputArg = addClerk(obj,inputArg)
+    %         %METHOD1 此处显示有关此方法的摘要
+    %         %   此处显示详细说明
+    %         outputArg = obj.Property1 + inputArg;
+    %     end
+        function ticket_no = getTicket(obj,inputArg)
             %METHOD1 此处显示有关此方法的摘要
             %   此处显示详细说明
-            outputArg = obj.Property1 + inputArg;
+            ticket_no=obj.db_ticket;
+            obj.db_ticket=obj.db_ticket+1;
         end
     end
 end

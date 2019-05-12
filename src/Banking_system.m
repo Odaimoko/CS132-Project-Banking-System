2c58aa4019ae8fc24709e324ecb6d5dc66321ddc
oda <Odaimoko@gmail.com>
2019-05-12 11:10:01 +0800

    remove account

--- src/Banking_system.m
+++ src/Banking_system.m
@@ -28,6 +28,7 @@ classdef Banking_system
                 obj.db_account('nextAccountID')=1;
                 obj.db_account('nextCardID')=1;
                 obj.db_account('accounts')=containers.Map(0,struct); % person id maps to list of cards
+                remove(obj.db_account('accounts'),0)
                 obj.db_ticket=0;
             end
             % initialize ticket account in mem
@@ -41,7 +42,8 @@ classdef Banking_system
             account=struct;
             account.accoutID=obj.db_account('nextAccountID');
             account.personID=person_id;
-            account.cards=containers.Map;
+            account.cards=containers.Map(0,0);
+            remove(account.cards,0);
             account.password=password; %????
             obj.db_account('nextAccountID')=obj.db_account('nextAccountID')+1;
 
@@ -49,17 +51,16 @@ classdef Banking_system
             A(account.accoutID)=account;
             obj.db_account('accounts')=A;
             save 'data/sys.mat' obj
-            outputArg=true;
+            outputArg=account.accoutID;
         end
 
 
         function outputArg = addCard(obj,person_id,account_id,password)
             A=obj.db_account('accounts');
-            if ~isKey(A,account_id)
-                outputArg=false;
-            else
+            outputArg=false;
+            if isKey(A,account_id)
                 account=A(account_id);
-                if strcmp(password,account.password) && strcmp(person_id,account.personID)
+                if strcmp(password,account.password) && person_id==account.personID
                     cardID=obj.db_account('nextCardID');
                     obj.db_account('nextCardID')=obj.db_account('nextCardID')+1;
 
@@ -68,45 +69,22 @@ classdef Banking_system
                     account.cards=cards;
                     A(account_id)=account;
                     save 'data/sys.mat' obj
-                    outputArg=true;
-                else
-                    outputArg=false;
+                    outputArg=cardID;
                 end
             end
         end
 
-        function outputArg = removeCard(obj,card_id,person_id,password)
+
+        function outputArg = removeAccount(obj,person_id,account_id,password)
             outputArg=false;
-
             A=obj.db_account('accounts');
             if isKey(A,person_id)
                 account=A(person_id);
-                if strcmp(password,account.password)
-                    if isKey(card_id,account.cards)
-                        remove(account.cards,card_id);
-                        outputArg=true;
-                        A(person_id)=account;
-                    end
-                    % remove(obj.db_account('accounts'),person_id);
-                end
-            else
-            end
-        end
-
-        function outputArg = removeAccount(obj,person_id,password)
-
-            A=obj.db_account('accounts');
-            if isKey(A,person_id)
-                account=A(person_id);
-                if strcmp(password,account.password)
-                    remove(obj.db_account('accounts'),person_id);
+                if strcmp(password,account.password) && person_id==account.personID
+                    remove(obj.db_account('accounts'),account_id)
+                    save 'data/sys.mat' obj
                     outputArg=true;
-                else
-                    % pw wrong
-                    outputArg=false;
                 end
-            else
-                outputArg=false;
             end
         end
 
